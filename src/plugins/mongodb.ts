1a3e2cc6c800642b35fa98d59736a5ba1b65dab1
Hubert Lipiński <hubertlipinskipl@gmail.com>
2025-05-09 18:24:38 +0200

    init

---
+++ src/plugins/mongodb.ts
@@ -0,0 +1,13 @@
+import fp from 'fastify-plugin'
+import fastifyMongo from '@fastify/mongodb'
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+export default fp(async (fastify, opts) => {
+    fastify.register(fastifyMongo, {
+        forceClose: true,
+        url: `${process.env.MONGODB_URI}`,
+    })
+})
+
--- src/routes/root.ts
+++ src/routes/root.ts
@@ -2,7 +2,11 @@ import { FastifyPluginAsync } from 'fastify'
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get('/', async function (request, reply) {
-    return { root: true }
+
+    // @ts-ignore
+    const data = fastify.mongo.db.collection('users').findOne({})
+
+    return data
   })
 }
 
